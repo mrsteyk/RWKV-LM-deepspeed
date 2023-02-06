17a4eb9af0928ebbcc8d02511d49ad0143d5703d
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2023-02-06 18:45:06 +0500

    safetensors rnn
    This allows to just mmap and use tensors as is?

--- convert_safetensors.py
+++ convert_safetensors.py
@@ -2,7 +2,41 @@ import argparse
 import os
 
 import torch
-from safetensors.torch import save_file
+from safetensors.torch import save_file, load_file
+
+def convert_rnn(w, float_mode="fp32", rescale_layer=6):
+    # RWKV_RESCALE_LAYER = 6
+    # refine weights and send to correct device
+    keys = list(w.keys())
+    if 'pos_emb_x' in keys:
+        w['pos_emb'] = (w['pos_emb_x'] + w['pos_emb_y']).reshape(args.ctx_len+1, -1)[:-1,:]
+    keys = list(w.keys())
+    for x in keys:
+        block_id = 0
+        if 'blocks.' in x:
+            block_id = int(x.split('.')[1])
+        if 'att.output.weight' in x:
+            w[x] = w[x] / (2 ** int(block_id // rescale_layer))
+        if 'ffn.value.weight' in x:
+            w[x] = w[x] / (2 ** int(block_id // rescale_layer))
+                        
+        if '.time_' in x:
+            w[x] = w[x].squeeze()
+        if '.time_decay' in x:
+            w[x] = w[x].float()
+            w[x] = -torch.exp(w[x])
+        elif '.time_first' in x:
+            w[x] = w[x].float()
+        else:
+            if float_mode == "fp32":
+                w[x] = w[x].float()
+            elif float_mode == "bf16":
+                w[x] = w[x].bfloat16()
+            elif float_mode == "fp16":
+                w[x] = w[x].half()
+
+        w[x].requires_grad = False
+    return w
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -11,6 +45,7 @@ def get_args():
     parser.add_argument("--zero_tag", type=str, required=False)
     parser.add_argument("--fp16", action='store_true')
     parser.add_argument("--bf16", action='store_true')
+    parser.add_argument("--rnn", action='store_true')
     args = parser.parse_args()
     return args
 
@@ -20,7 +55,10 @@ if __name__ == "__main__":
         from deepspeed.utils.zero_to_fp32 import get_fp32_state_dict_from_zero_checkpoint
         d = get_fp32_state_dict_from_zero_checkpoint(args.load_model_init, tag=args.zero_tag)
     else:
-        d = torch.load(args.input_checkpoint, map_location='cpu')
+        if args.input_checkpoint.endswith(".safetensors"):
+            d = load_file(args.input_checkpoint)
+        else:
+            d = torch.load(args.input_checkpoint, map_location='cpu')
     if list(d.keys())[0].startswith("_forward_module."):
         d = {n[len("_forward_module."):]: d[n] for n in d.keys()}
     if args.output is None:
@@ -28,9 +66,14 @@ if __name__ == "__main__":
     elif not args.output.endswith("safetensors"):
         args.output = f"{args.output}s" if args.output.endswith("safetensor") else f"{args.output}.safetensors"
     
-    if args.fp16:
-        d = {k: v.half() for k, v in d.items()}
-    elif args.bf16:
-        d = {k: v.bfloat16() for k, v in d.items()}
+    if not args.rnn:
+        if args.fp16:
+            d = {k: v.half() for k, v in d.items()}
+        elif args.bf16:
+            d = {k: v.bfloat16() for k, v in d.items()}
+        else:
+            d = {k: v.float() for k, v in d.items()}
+    else:
+        d = convert_rnn(d, float_mode="fp16" if args.fp16 else "bf16" if args.bf16 else "fp32")
 
     save_file(d, args.output)
