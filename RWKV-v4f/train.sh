89a632ff61094b4d6629d4daa379b27e296d3400
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2023-03-30 01:24:09 +0500

    SFT is weird...

--- RWKV-v4f/train.sh
+++ RWKV-v4f/train.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 train.py --data_file train.npy --vocab_size 50277 --vocab_size_delta 2 --ctx_len 1024 --batch_size 1 --n_layer 24 --n_embd 1024 --head_qk 0 --lr_init 1e-5 --lr_final 1e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.999 --adam_eps 1e-8
+python3 train.py --load_model_init ../RWKV-v4neo/RWKV-4-Pile-430M-20220808-8066.safetensors --data_file train.npy --vocab_size 50277 --vocab_size_delta 2 --ctx_len 1024 --batch_size 1 --n_layer 24 --n_embd 1024 --head_qk 0 --lr_init 1e-5 --lr_final 1e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.999 --adam_eps 1e-8
